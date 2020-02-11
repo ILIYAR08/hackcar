0c3a729a7012a6a529eeac3590042f4b9a337700
Iliya Ramadanovic <iliya.ramadanovic@gmail.com>
2020-02-11 16:27:53 +0000

    more sensor stuff

--- motorinput.py
+++ motorinput.py
@@ -20,9 +20,15 @@ while True:
     if input1=='back':
         GPIO.output(3, True)    #motor 1
         GPIO.output(5, False)   #motor 1
-        
         GPIO.output(7, True)    #enable motor 1
-        sleep(.5)
+        
+        for a in range (5):
+                    if distance()<5:
+                        break
+                    else:
+                        sleep(.1) 
+        
+        
         GPIO.output(3, False)
         GPIO.output(5, False) #disable motor 1
         GPIO.output(7, False)
@@ -51,7 +57,14 @@ while True:
                 GPIO.output(11, True)    #motor 1
                 GPIO.output(13, False)   #motor 1
                 GPIO.output(15, True) 
-                sleep(.5)
+                
+                for a in range (5):
+                    if distance()<5:
+                        break
+                    else:
+                        sleep(.1) 
+                
+                
                 GPIO.output(3, False)
                 GPIO.output(5, False) #disable motor 1
                 GPIO.output(7, False)
@@ -66,7 +79,13 @@ while True:
                     GPIO.output(11, False)    #motor 1
                     GPIO.output(13, True)   #motor 1
                     GPIO.output(15, True) 
-                    sleep(.5)
+                    
+                    for b in range (5):
+                    if distance()<5:
+                        break
+                    else:
+                        sleep(.1)
+                    
                     GPIO.output(3, False)
                     GPIO.output(5, False) #disable motor 1
                     GPIO.output(7, False)
