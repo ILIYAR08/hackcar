3bc2928da3948eb8a6b7c7b52f2e461ed5a74167
Iliya Ramadanovic <iliya.ramadanovic@gmail.com>
2020-02-11 16:16:06 +0000

    add sensor stuff

--- motorinput.py
+++ motorinput.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 from time import sleep
+from sensortest import *
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(3, GPIO.OUT)
 GPIO.setup(5, GPIO.OUT)
@@ -7,15 +8,16 @@ GPIO.setup(7, GPIO.OUT)
 GPIO.setup(11, GPIO.OUT)
 GPIO.setup(13, GPIO.OUT)
 GPIO.setup(15, GPIO.OUT)
+
+
 while True:
+    print (distance())
+    
+   
+     
+        
     input1=input('put in a word: ')
     if input1=='back':
-        
-        
-       
-        
-       
-        
         GPIO.output(3, True)    #motor 1
         GPIO.output(5, False)   #motor 1
         
@@ -27,13 +29,17 @@ while True:
         
 
     else:
-        if input1=='go':
+        if input1=='go'and distance()>10:
             
             GPIO.output(3, False)    
             GPIO.output(5, True)   
             GPIO.output(7,True)
            
-            sleep(.5)
+            for n in range (5):
+                if distance()<5:
+                    break
+                else:
+                    sleep(.1) 
             GPIO.output(3, False)
             GPIO.output(5, False)
             GPIO.output(7,False)
@@ -68,8 +74,8 @@ while True:
                     GPIO.output(13, False)   #motor 1
                     GPIO.output(15, False) 
 GPIO.cleanup()
-
+    
+                
+                
             
             
-        
-        
